eba493f06cb666426b4a339504de447303f11eef
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-02-01 12:05:29 +0100

    Subida inicial del proyecto

---
+++ src/auth/pages/index.js
@@ -0,0 +1,3 @@
+// Archivo de barril donde estan los archivos que estan dentro del directorio pages, que esta dentro del directorio auth
+export * from './LoginPage';
+export * from './RegisterPage';
---
+++ src/theme/index.js
@@ -0,0 +1,3 @@
+//Archivo de barril donde estan los archivos que estan dentro del directorio theme
+export * from './AppTheme';
+export * from './purpleTheme';
