1367ee6773779089b955645229931c415911ad71
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-02-03 19:23:39 +0100

    Actualizacion de nustro custom hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export const useForm = ( initialForm = {}, formValidations = {}) => {
   
@@ -9,6 +9,15 @@ export const useForm = ( initialForm = {}, formValidations = {}) => {
         createValidators();
     }, [ formState ])
     
+    const isFormValid = useMemo( () => {
+
+        for (const formValue of Object.keys( formValidation )) {
+            if ( formValidation[formValue] !== null ) return false;
+            
+        }
+        
+        return true;
+    }, [ formValidation ])
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -34,6 +43,7 @@ export const useForm = ( initialForm = {}, formValidations = {}) => {
         }
 
         setFormValidation( formCheckedValues );
+        console.log(formCheckedValues);
 
     }
 
@@ -44,5 +54,6 @@ export const useForm = ( initialForm = {}, formValidations = {}) => {
         onResetForm,
 
         ...formValidation,
+        isFormValid
     }
 }
