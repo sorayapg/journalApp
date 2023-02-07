9c940ec24db80b2bb6e36113600ff1ac8e2de553
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-02-07 18:22:01 +0100

    Mantenimiento del estado

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,54 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Navigate, Route, Routes } from "react-router-dom";
+import { FirebaseAuth } from "../firebase/config";
+
+import { login, logout } from "../store/auth";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+
+
+    const { status } = useSelector( state => state.auth);
+    const diapatch = useDispatch();
+
+    useEffect(() => {
+
+        onAuthStateChanged( FirebaseAuth, async( user ) => {
+
+            if ( !user ) return diapatch( logout() );
+
+            const { uid, email, displayName, photoURL } = user;
+            diapatch( login({ uid, email, displayName, photoURL }) );
+
+        })
+    }, []);
+    
+    if ( status === 'checking') {
+        return <CheckingAuth />
+    }
+
     return (
         <Routes>
+
+            {
+                (status === 'authenticated')
+                ? <Route path="/*" element={ <JournalRoutes />} />
+                : <Route path="/auth/*" element={ <AuthRoutes />} />
+            
+            }
+
+            <Route path="/*" element={ <Navigate to="/auth/login" /> }/>
+
             {/* Loguin y Registro */}
-            <Route path="/auth/*" element={ <AuthRoutes />} />
+            {/* <Route path="/auth/*" element={ <AuthRoutes />} /> */}
             {/* JournalApp */}
-            <Route path="/*" element={ <JournalRoutes />} />
+            {/* <Route path="/*" element={ <JournalRoutes />} /> */}
             
         </Routes>
     )
---
+++ src/ui/index.js
@@ -0,0 +1,2 @@
+// Archivo de barril donde estaan los archivos que estan dentro del directorio ui
+export * from './components/CheckingAuth';
