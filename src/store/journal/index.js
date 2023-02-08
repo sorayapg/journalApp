e68c87ce0905fdba6794f34e1d2753ebbac49a2a
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-02-08 10:51:28 +0100

    Actualizacion del Journal

---
+++ src/store/journal/index.js
@@ -0,0 +1,4 @@
+// Archivo de barril donde estan los archivos que estan dentro del directorio journal, que esta dentro del directorio store
+
+export * from './journalSlice';
+export * from './thunks';
