65aa7afa50abac4c1b2f9868154e100a09989cd2
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-02-08 13:26:36 +0100

    Actualizacion en la base de datos

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,19 @@
+import { useDispatch } from "react-redux";
 import { IconButton } from "@mui/material";
 import { AddOutlined } from "@mui/icons-material";
 import { JourmalLayout } from "../layout/JourmalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { startNewNote } from "../../store/journal";
 
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch();
+
+    const onClickNewNote = () => {
+        dispatch( startNewNote() );
+    }
+
     return (
         <JourmalLayout>
            { /* <Typography>Nostrud nostrud dolor cupidatat ullamco laborum quis officia officia mollit officia. Incididunt et ullamco cupidatat consectetur Lorem do mollit pariatur do nisi veniam adipisicing ad pariatur. Anim sit laboris aliqua et. Et mollit labore exercitation laborum est eu ut incididunt ut cillum. Enim non eiusmod quis duis magna. In pariatur sit labore do irure excepteur occaecat ea eiusmod excepteur consectetur incididunt irure Lorem.</Typography> */}
@@ -13,6 +22,7 @@ export const JournalPage = () => {
             {/* <NoteView /> */ }
 
             <IconButton
+                onClick={ onClickNewNote }
                 size='large'
                 sx={{
                     color: 'white',
